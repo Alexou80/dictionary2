d21bd2ccb3c6839282cf499a3070720686fb626c
Alexou80 <84702060+Alexou80@users.noreply.github.com>
2021-06-07 15:27:49 +0200

    debuging

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 
 import "./App.css";
 import Dictionary from "./Dictionary";
-
+import logo from "./logo.png";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          
         </header>
         <main>
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by SheCodes</small>
+          <small>Coded by Alexandra</small>
         </footer>
       </div>
     </div>
