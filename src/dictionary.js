aba3b6546dc80e9051241229aa0a8544f3bb425c
Alexou80 <84702060+Alexou80@users.noreply.github.com>
2021-06-10 10:57:42 +0200

    Update dictionary.js

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+
 import "./dictionary.css";
+
 export default function Dictionary() {
+
   let [keyword, setKeyword] = useState("");
+
   function search(event) {
+    function handelresponse(response){}
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+
+    let apiurl='https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}';
+    axios. get(apiUrl).then(handelresponse);
   }
+  
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
