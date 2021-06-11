e28ebed692cd0af1ac7a459a48657ccf458fbd2d
Alexou80 <84702060+Alexou80@users.noreply.github.com>
2021-06-11 14:00:02 +0200

    Create phonetics.js

---
+++ src/phonetics.js
@@ -0,0 +1,15 @@
+import React from "react";
+export default function Phonetics (props) {
+
+    console.log(props.phonetics);
+    return (<div className="Phonetics">
+            {props.phonetics.map(function(phonetic,index){
+                    return(
+                            <div key={index}>
+                                    <p>{phonetic.text}</p>
+                                    <a href={phonetic.audio} target="_blank" rel="noreferrer">Listen</a>
+                        </div>
+                                  
+                    )
+            })}           
+        </div>)
