87a065eefe642e270ec6b463393faf009d0b8c6d
Alexou80 <84702060+Alexou80@users.noreply.github.com>
2021-06-12 09:28:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
-    <h1 className:"title"> Dictionary</h1>
+        <header className="App-header" Dictionary></header>
+   
         <main>
           <Dictionary />
         </main>
