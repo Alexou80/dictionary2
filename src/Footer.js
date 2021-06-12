29f8ef56b2505470e5de386a183abacbf43db80a
Alexou80 <84702060+Alexou80@users.noreply.github.com>
2021-06-12 14:35:05 +0200

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,26 +3,10 @@ import "./Footer.css";
 
 export default function Footer() {
 	return (
-		<footer className="Footer pt-2">
+		<footer className="Footer-react">
 			This project was coded by{" "}
 			<p> Alexandra Philips</p>
-			. It is
-			<a
-				href="https://github.com/Alexou80/dictionary2"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				{" "}
-				open-sourced on GitHub
-			</a>{" "}
-			and{" "}
-			<a
-				href="https://naughty-meitner-3cbe5c.netlify.app/"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				hosted on Netlify.
-			</a>
+			
 		</footer>
 	);
 }
