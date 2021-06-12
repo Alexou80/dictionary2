10f65fc40c55de046e98306789b6fbeeb7b30132
Alexou80 <84702060+Alexou80@users.noreply.github.com>
2021-06-12 12:07:30 +0200

    footer

---
+++ src/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+	return (
+		<footer className="Footer pt-2">
+			This project was coded by{" "}
+			<p> Alexandra Philips</p>
+			. It is
+			<a
+				href="https://github.com/ozlemalp/react-dictionary-app"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				{" "}
+				open-sourced on GitHub
+			</a>{" "}
+			and{" "}
+			<a
+				href="https://naughty-meitner-3cbe5c.netlify.app/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				hosted on Netlify.
+			</a>
+		</footer>
+	);
+}
